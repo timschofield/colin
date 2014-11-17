5d8bfc318b2dea505490220f4911ef977ca6a019
Tim Schofield <tim@weberpafrica.com>
2014-11-17 11:58:17 +0000

    Improvements to the results screen to allow selection of the date of the test

--- public_html/index.php
+++ public_html/index.php
@@ -4,6 +4,10 @@ include('../includes/config.php');
 
 $db = mysqli_connect($Host, $DBUser, $DBPassword, 'colin', $DBPort);
 
+if (!isset($_POST['SelectedDate'])) {
+	$_POST['SelectedDate'] = date('Y-m-d');
+}
+
 $SQL = "SELECT runtime,
 				tests.testnumber,
 				description,
@@ -12,7 +16,9 @@ $SQL = "SELECT runtime,
 				testoutput
 			FROM tests
 			INNER JOIN outputs
-				ON tests.testnumber=outputs.testnumber";
+				ON tests.testnumber=outputs.testnumber
+			WHERE DATE(runtime)='" . $_POST['SelectedDate'] . "'
+			ORDER BY runtime, testnumber";
 $Result = mysqli_query($db, $SQL);
 
 echo '<!DOCTYPE html>
@@ -27,6 +33,20 @@ echo '<body>
 		<img src="Colin.png" />
 		<span id="title">Colin\'s Results Page</span>';
 
+$DatesSQL = "SELECT DISTINCT DATE(runtime) FROM outputs";
+$DatesResult = mysqli_query($db, $DatesSQL);
+echo '<form id="DateForm" method="post" action="', htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, 'UTF-8'), '" >';
+echo 'Select Date of Tests:   ';
+echo '<select name="SelectedDate" onchange="document.forms[\'DateForm\'].submit();">';
+while ($DatesRow = mysqli_fetch_row($DatesResult)) {
+	if ($DatesRow[0] == $_POST['SelectedDate']) {
+		echo '<option selected="selected" value="' . $DatesRow[0] . '">' . $DatesRow[0] . '</option>';
+	} else {
+		echo '<option value="' . $DatesRow[0] . '">' . $DatesRow[0] . '</option>';
+	}
+}
+echo '</select>
+		</form>';
 echo '<table>
 		<tr>
 			<th>Message Time</th>
